7219cf916d59b8bc33a4ea0b7791f56dcaa6ee22
Panadestein <rpana92@gmail.com>
2023-09-10 11:30:25 +0200

    Set a nice development prompt

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   description = "An HIP development flake";
+  nixConfig.bash-prompt = ''\033[1;31m[\033[0m\033[1;37mdev .\033[0m\033[1;34m $(basename \$$PWD)\033[0m\033[1;31m]\033[0m '';
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
